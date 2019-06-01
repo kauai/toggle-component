b2ab5dafbeafe1ed1cea8459805763c3060b6a1c
Thiago kauai <html5ephp@gmail.com>
2019-06-01 00:51:32 -0300

    recriando componentes - component-toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Toggle = ({ defaulValue, onChange }) => {
+  const [ checked, setChecked ] = useState(defaulValue)
+  // console.log('CHECKED',checked)
+  const onToggle = () => {
+    setChecked(!checked)
+    if(onChange){
+      onChange(!checked)
+    }
+  }
+   return <div className={`toggle ${checked && 'toggle-checked'}`} onClick={onToggle}></div>
+}
+
 function App() {
+  const [ checked2, setChecked2 ] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img width="150" src={logo} className="App-logo" alt="logo" />
+         <Toggle defaulValue/>
+         <Toggle onChange={value => setChecked2(value)}/>
+         <p>Checked2 {JSON.stringify(checked2)}</p>
       </header>
     </div>
   );
